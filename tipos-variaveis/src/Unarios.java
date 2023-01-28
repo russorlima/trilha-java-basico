a8cfbf8ef45951ecb67760d05da05963a6b5e624
Russor <107722401+russorlima@users.noreply.github.com>
2023-01-28 18:35:49 -0300

    Unarios

---
+++ tipos-variaveis/src/Unarios.java
@@ -0,0 +1,20 @@
+public class Unarios {
+    public static void main(String[] args) {
+        
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = + numero * -1;
+
+        System.out.println(numero);
+
+
+
+
+
+    }
+    
+}
