9d4fd271ccc2206585696a4b3598aa3ea4486049
Russor <107722401+russorlima@users.noreply.github.com>
2023-01-28 19:30:35 -0300

    Operadores Logicos

---
+++ tipos-variaveis/src/OperadoresLogicos.java
@@ -0,0 +1,18 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        
+        boolean condicao1 = true;
+
+        boolean condicao2 = true;
+
+        if(condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("as duas condições são verdadeiras");
+        
+    }
+         System.out.println("Fim");
+}
+}
