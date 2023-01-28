f5ed130a3137c79f66490992c311d516ed22f802
Russor <107722401+russorlima@users.noreply.github.com>
2023-01-28 18:46:52 -0300

    operador de incremento

---
+++ tipos-variaveis/src/Incremento.java
@@ -0,0 +1,15 @@
+public class Incremento {
+
+    public static void main(String[] args) {
+        
+    
+
+    int numero = 5;
+
+    System.out.println(numero --);
+    
+    System.out.println(numero);
+
+    }
+    
+}
