5292eb6a834c3dc46de8bea18aab8629bfd6d277
Russor <107722401+russorlima@users.noreply.github.com>
2023-01-29 12:33:39 -0300

    comentários

---
+++ Comentarios/src/comentarios-projeto.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Olá, eu sou um comentário de uma linha
+
+        /*
+         * Olá
+         * Eu sou um comentário mais detalhado 
+         * caso necessário.
+         */
+
+
+    }
+    public void metodo(){
+        /**
+         * Estas duas estrelinhas acima
+         * é para identificar que voc~e
+         * pretende elaborar um comentário
+         * a nível de documentação.
+         */
+
+    }
+}
--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -3,6 +3,7 @@ public class SmartTv {
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
+    
     public void mudarCanal (int novoCanal){
         canal = novoCanal;
     }
